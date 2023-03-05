0dba10692dc4c4ef44479470b9379427e8e33dfd
Monika Kupracz <kupraczmonika@gmail.com>
2023-03-05 20:18:49 +0100

    Add varibles js

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,9 @@
-let computerMove = `kamień`;
+function printMessage(msg) {
+  let div = document.createElement("div");
+  div.innerHTML = msg;
+  document.getElementById("messages").appendChild(div);
+}
 
-printMessage(
-  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
-);
-
-let playerMove = `papier`;
-
-printMessage(
-  "Zagrałem " + playerMove + "! Jeśli Twój ruch to nożyce, to wygrywasz!"
-);
+function clearMessages() {
+  document.getElementById("messages").innerHTML = "";
+}
